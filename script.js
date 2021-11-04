656b35c3dc008640d45b3f39d2ef80dc6d32cd0b
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2021-11-04 13:40:20 +0100

    Filtering cities

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-// const options = {
-//   method: 'GET',
-//   url: 'https://api.schiphol.nl/public-flights/destinations?page=4',
-//   headers: {
-//     Accept: "application/json",
-//     app_id: "2754d242",
-//     app_key: "67b1ecc17f4097520677f888b201d009",
-//     ResourceVersion: "v4",
-//   }
-// };
-
-const getDestinations = async () => {
-  const response = await fetch("https://api.schiphol.nl/public-flights/destinations?page=4",{
+export const getDestinations = async () => {
+  const response = await fetch("https://api.schiphol.nl/public-flights/destinations?page=68",{
     method: "GET",
     headers: {
       Accept: "application/json",
@@ -22,33 +11,52 @@ const getDestinations = async () => {
 
   const allData = await response.json();
   const destinations = allData.destinations
+  // console.log(allData);
   destinations.map(singleData => {
     const publicName = singleData.publicName.english
-    console.log(publicName);
+    publicNameArray.push(publicName)  
   });
-  
+  destinations.map(singleData => {
+    let city = singleData.city
+    if (typeof city === "string") {
+      console.log(city);
+    } else {
+      // delete singleData
+    }
+    // console.log(city);
+    // citiesArray.push(city)  
+    // if(typeof city == !'string') {
+    //   city = 'kak'
+    // }
+    // citiesArray.map(city => {
+    //   // console.log(city);
+    // console.log(citiesArray["Athens"]);
+    //   if(city === null) {
+    //     delete citiesArray[city]
+    //   }
+    // })
+  });
+  // console.log(publicNameArray);
 }
+ getDestinations()
 
-getDestinations()
+export let publicNameArray = []
+// console.log(publicNameArray);
 
+let citiesArray = []
 
-// axios.request(options).then((response) => {
-// 	// console.log(response.data); 
-//   const dataResponse = response.data;
-//   const data = dataResponse.destinations
-//   data.map(singleData => {
-//      const publicName = singleData.publicName.english
-//      console.log(publicName);
-
-//   })
-  
+// citiesArray.map(city => {
+//   console.log(city);
+//   // if(typeof city == null) {
+//   //   return city = 'kak'
+//   // }
 // })
-// .then((response) => {
+// console.log(citiesArray);
+// import {getCoordinate} from './getlocation.js'
 
-// })
-// .catch(function (error) {
-// 	console.error(error);
-// });
+// getCoordinate()
+
+
 
 
 
